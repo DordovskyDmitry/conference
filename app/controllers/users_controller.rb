1ce3bc9d6756305e40c2d7621427f833f43b8d94
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-06 19:30:59 +0200

    add action to users controller and fake method require_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+  before_filter :require_admin, :only => [:show, :edit, :update, :destroy]
+  before_filter :get_user, :only => [:show, :edit, :update, :destroy]
+  before_filter :get_users, :only => [:index, :program]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      flash[:notice] = t 'flash.add_user'
+      redirect_to root_url
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:notice] = t 'flash.update_user'
+      redirect_to root_url
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_url
+  end
+
+  def program
+  end
+
+  private
+
+  def get_user
+    @user = User.find params[:id]
+  end
+
+  def get_users
+    @users = User.all
+  end
 end
