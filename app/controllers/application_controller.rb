d799fd7ffde6e7ba69f7bbb464a201684d719394
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-13 18:13:14 +0200

    Added I18n.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :authenticated?
+  before_filter :save_locale
+  before_filter :set_locale
 
   def require_admin
     redirect_to root_url unless authenticated? 
@@ -8,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   def authenticated?
     session[:admin] == 'admin'
+  end
+
+  def set_locale
+    I18n.locale = cookies[:lang] || :en
+  end
+
+  def save_locale
+    cookies[:lang] = params[:lang] if (params[:lang] != cookies[:lang]) && ['ru','en'].include?(params[:lang])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def lang_menu(this_page)
+    ['ru','en'].each do |lang|
+      concat link_to(lang, "#{this_page.gsub(/(\?|\/|\&)lang=\w*(\&|$)/,'')}?lang=#{lang}") + " "
+    end
+  end
 end
