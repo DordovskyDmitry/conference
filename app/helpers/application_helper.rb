cef95953702195a520affe426d7421ae69e73839
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-19 23:22:41 +0200

    Login/logout link. Basic css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@ module ApplicationHelper
 
   def lang_menu(this_page)
     ['ru','en'].each do |lang|
-      concat link_to(lang, "#{this_page.gsub(/(\?|\/|\&)lang=\w*(\&|$)/,'')}?lang=#{lang}") + " "
+      concat content_tag :span, link_to(lang, "#{this_page.gsub(/(\?|\/|\&)lang=\w*(\&|$)/,'')}?lang=#{lang}") 
+    end
+  end
+
+  def login_url
+    if authenticated?
+      concat link_to(t('auth.logout'), user_sessions_path, :method => :delete)
+    else
+      concat link_to(t('auth.login'), new_user_sessions_path)
     end
   end
 end
