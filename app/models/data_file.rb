a0a78f086c351ed210ab78330e42a7ebaf9203fd
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-21 22:10:32 +0200

    Some problem

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -2,13 +2,15 @@ class DataFile
   #require 'rutils'
 
   def self.save(upload)
-    spl = upload.original_filename.split('.')
-    ext = spl[spl.length-1]
-    name = File.basename(upload.original_filename).gsub(/.#{ext}/, '').dirify<<'.'<<ext
-    directory = "public"
-    path = File.join(directory, name)
-    File.open(path, "wb") { |f| f.write(upload.read) }
-    return name
+    File.open("#{RAILS_ROOT}/public/asd.pdf", "w+") { |f| f.write(upload.read); f.close }
+    return 1
+    #spl = upload.original_filename.split('.')
+    #ext = spl[spl.length-1]
+    #name = File.basename(upload.original_filename).gsub(/.#{ext}/, '').dirify<<'.'<<ext
+    #directory = "public"
+    #path = File.join(directory, name)
+    #File.open(path, "wb") { |f| f.write(upload.read) }
+    #return name
   end
 
 end
