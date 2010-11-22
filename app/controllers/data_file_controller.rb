5723efaab827d56b89bdf5019f5e53cafceb9003
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-22 21:39:38 +0200

    Upload files

--- app/controllers/data_file_controller.rb
+++ app/controllers/data_file_controller.rb
@@ -3,8 +3,11 @@ class DataFileController < ApplicationController
   def upload
     begin
       DataFile.save(params[:upload])
+      p params[:upload]
+      p "save"
     rescue
       flash[:error] = 'sdf'
+      p "not save"
     end
     redirect_to root_path
   end
--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,16 +1,13 @@
 class DataFile
-  #require 'rutils'
 
   def self.save(upload)
-    File.open("#{RAILS_ROOT}/public/asd.pdf", "w+") { |f| f.write(upload.read); f.close }
-    return 1
-    #spl = upload.original_filename.split('.')
-    #ext = spl[spl.length-1]
-    #name = File.basename(upload.original_filename).gsub(/.#{ext}/, '').dirify<<'.'<<ext
-    #directory = "public"
-    #path = File.join(directory, name)
-    #File.open(path, "wb") { |f| f.write(upload.read) }
-    #return name
+    spl = upload.original_filename.split('.')
+    ext = spl[spl.length-1]
+    name =  File.basename(upload.original_filename).gsub(/.#{ext}/,'')<<'.'<<ext
+    directory = "public"
+    path = File.join(directory, name)
+    File.open(path, "wb") { |f| f.write(upload.read) }
+    return name
   end
 
 end
