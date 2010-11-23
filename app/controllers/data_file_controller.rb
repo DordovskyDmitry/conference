902344d2ebab3abad7a19dff05cad72930becfcb
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-23 19:53:45 +0200

    Cleanup

--- app/controllers/data_file_controller.rb
+++ app/controllers/data_file_controller.rb
@@ -3,11 +3,8 @@ class DataFileController < ApplicationController
   def upload
     begin
       DataFile.save(params[:upload])
-      p params[:upload]
-      p "save"
     rescue
       flash[:error] = 'sdf'
-      p "not save"
     end
     redirect_to root_path
   end
