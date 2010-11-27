b8675dbf31ccaee8e4ffbf404bba235ca005bca8
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-11-27 13:29:10 +0200

    Add methods to data_file model

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,13 +1,23 @@
 class DataFile
 
+  DIRNAME = "public/pdf"
+
   def self.save(upload)
     spl = upload.original_filename.split('.')
     ext = spl[spl.length-1]
     name =  File.basename(upload.original_filename).gsub(/.#{ext}/,'')<<'.'<<ext
-    directory = "public"
-    path = File.join(directory, name)
+    path = File.join(DIRNAME, name)
     File.open(path, "wb") { |f| f.write(upload.read) }
     return name
+  end
+
+  def self.delete_all_pdf
+    files = Dir.glob("public/pdf/*.pdf")
+    files.each{|f| File.delete(f)}
+  end
+
+  def self.get_first_pdf
+    Dir.glob("#{DIRNAME}/*.pdf").first
   end
 
 end
