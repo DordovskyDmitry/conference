83babc197c87f643dc3afaaddf9e461a46715c83
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2010-12-10 22:10:16 +0200

    Some changes

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,10 +7,10 @@ class Address < ActiveRecord::Base
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
                       :message => 'email must be valid'
 
-  validates_format_of :home_phone, :with => /^[0-9]{10}$/,
-                      :if => Proc.new { |o| !o.home_phone.blank? }
-  validates_format_of :work_phone, :with => /^[0-9]{10}$/,
-                      :if => Proc.new { |o| !o.work_phone.blank? }
+  #validates_format_of :home_phone, :with => /^[0-9]{10}$/,
+  #                    :if => Proc.new { |o| !o.home_phone.blank? }
+  #validates_format_of :work_phone, :with => /^[0-9]{10}$/,
+  #                    :if => Proc.new { |o| !o.work_phone.blank? }
 
 
   before_validation :delete_symbols_from_phone
@@ -18,6 +18,6 @@ class Address < ActiveRecord::Base
   private
 
   def delete_symbols_from_phone
-    [work_phone, home_phone].each { |p| p.gsub!(/\D+/, '') }
+    [work_phone].each { |p| p.gsub!(/\D+/, '') }
   end
 end
